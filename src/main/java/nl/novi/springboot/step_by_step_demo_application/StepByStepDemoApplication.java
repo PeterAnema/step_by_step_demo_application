5404d2133471ad8e8130055ef2608cb53ab5e661
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-03-23 23:04:13 +0100

    Springboot Starter Project - Spring Web

---
+++ src/main/java/nl/novi/springboot/step_by_step_demo_application/StepByStepDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.springboot.step_by_step_demo_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepByStepDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepByStepDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/springboot/step_by_step_demo_application/StepByStepDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.springboot.step_by_step_demo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepByStepDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
